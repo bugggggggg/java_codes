19e1f14a59a8ef82cbb1538311f52244cdc36056
w57 <44924074+bugggggggg@users.noreply.github.com>
2020-11-18 17:41:18 +0800

    Initial commit

---
+++ src/main/java/com/demo/common/lang/dto/RegisterDto.java
@@ -0,0 +1,21 @@
+package com.demo.common.lang.dto;
+
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class RegisterDto {
+
+    @NotBlank(message = "昵称不能为空")
+    private String username;
+
+    private String avatar;
+
+    @NotBlank(message = "密码不能为空")
+    private String password;
+
+    @NotBlank(message = "邮箱不能为空")
+    private String email;
+}
---
+++ src/main/java/com/demo/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.demo.controller;
+
+
+import com.demo.common.lang.Result;
+import com.demo.entity.User;
+import com.demo.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author w_57
+ * @since 2020-11-17
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequiresAuthentication
+    @GetMapping("/index")
+    public Result index() {
+        User user = userService.getById("1");
+        return Result.succ(user);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user){
+        userService.save(user);
+
+        return Result.succ(user);
+    }
+
+}
