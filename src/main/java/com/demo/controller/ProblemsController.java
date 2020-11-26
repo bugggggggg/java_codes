eafd06fe40b54cb8f281eb6e2df0a43895e37972
w57 <44924074+bugggggggg@users.noreply.github.com>
2020-11-26 22:56:47 +0800

    repair mistakes

--- src/main/java/com/demo/controller/ProblemsController.java
+++ src/main/java/com/demo/controller/ProblemsController.java
@@ -1,9 +1,22 @@
 package com.demo.controller;
 
 
+import cn.hutool.core.map.MapUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.demo.common.lang.Result;
+import com.demo.entity.Problems;
+import com.demo.entity.Submissions;
+import com.demo.service.ProblemsService;
+import com.demo.service.SubmissionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +27,44 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-11-26
  */
 @RestController
-@RequestMapping("/problems")
 public class ProblemsController {
+    @Autowired
+    ProblemsService problemsService;
 
+
+    @GetMapping("/problemList")
+    public Result getProblemList(@RequestParam("pagenum") int pagenum,
+                                 @RequestParam("pagesize")int pagesize,
+                                 @RequestParam("query") String query){
+        List<Problems> retList=new ArrayList<>();
+
+        int total=problemsService.count();
+        if(query.length()>0)
+        {
+            retList=problemsService.list(new QueryWrapper<Problems>().eq("problemId",query));
+        }
+        if(retList.size()==0)
+        {
+            List<Problems> problemsList=problemsService.list();
+
+            //String jsonProblemList= JSON.toJSONString(problemList);
+
+            for(int i=Math.min(total-1,(pagenum-1)*pagesize);i<Math.min(total,pagenum*pagesize);i++){
+                retList.add(problemsList.get(i));
+            }
+        }
+        else total=retList.size();
+        return Result.succ(MapUtil.builder()
+                .put("problemList",retList)
+                .put("total",total)
+                .map());
+    }
+
+
+    @GetMapping("/problem")
+    public Result getProblem(@RequestParam("problemId") String problemId){
+        Problems problem=problemsService.getById(problemId);
+
+        return Result.succ(problem);
+    }
 }
--- src/main/java/com/demo/controller/SubmissionsController.java
+++ src/main/java/com/demo/controller/SubmissionsController.java
@@ -1,18 +1,15 @@
 package com.demo.controller;
 
 
-import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.map.MapUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.demo.common.lang.Result;
 import com.demo.entity.Submissions;
 import com.demo.service.SubmissionsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,13 +28,40 @@ public class SubmissionsController {
     SubmissionsService submissionsService;
 
 
-    @GetMapping("/status")
-    public Result getSubmissionStatus(){
-        List<Submissions> submissionsList=submissionsService.list();
+    @GetMapping("/statusList")
+    public Result getProblemList(@RequestParam("pagenum") int pagenum,
+                                 @RequestParam("pagesize")int pagesize,
+                                 @RequestParam("query") String query){
+        List<Submissions> retList=new ArrayList<>();
 
+        int total=submissionsService.count();
+        if(query.length()>0)
+        {
+            retList=submissionsService.list(new QueryWrapper<Submissions>().eq("problemId",query));
+        }
+        if(retList.size()==0)
+        {
+            List<Submissions> submissionsList=submissionsService.list();
 
+            //String jsonProblemList= JSON.toJSONString(problemList);
+
+            for(int i=Math.min(total-1,(pagenum-1)*pagesize);i<Math.min(total,pagenum*pagesize);i++){
+                retList.add(submissionsList.get(i));
+            }
+        }
+        else total=retList.size();
         return Result.succ(MapUtil.builder()
-                .put("submissionsList",submissionsList)
+                .put("statusList",retList)
+                .put("total",total)
+                .map());
+    }
+
+
+    @GetMapping("/status")
+    public Result getProblem(@RequestParam("userId") String userId){
+        List<Submissions> retList=submissionsService.list(new QueryWrapper<Submissions>().eq("uid",userId));
+        return Result.succ(MapUtil.builder()
+                .put("statusList",retList)
                 .map());
     }
 
