d28a4a8e907c7c4be4fda452dedc943d3c4ae765
w57 <44924074+bugggggggg@users.noreply.github.com>
2020-12-16 01:31:10 +0800

    add contest

--- submitFile/cplusplus/1.cpp
+++ submitFile/cplusplus/1.cpp
@@ -1,8 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int x,y;
-	scanf("%d%d",&x,&y);
-	print("%d\n%d",x+y,x-y);
-	return 0;}
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    printf("%d\n%d\n",x+y,x-y);
+    return 0;
+}
